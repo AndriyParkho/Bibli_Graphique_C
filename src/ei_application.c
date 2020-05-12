4935bd820b6b9e1c1af23e89b825e51c89d939e0
Thibault <thibault.launay@grenoble-inp.org>
2020-05-12 16:26:23 +0200

    A1

--- src/ei_application.c
+++ src/ei_application.c
@@ -1,18 +1,31 @@
-#include ""
+#include "../include/ei_application.h"
 
-void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen);
+void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen) {
+        hw_init();
+        hw_create_window();
+        hw_quit;
+}
 
+void ei_app_free(void) {
 
-void ei_app_free(void):
+}
 
-void ei_app_run(void):
+void ei_app_run(void) {
 
-void ei_app_invalidate_rect(ei_rect_t* rect):
+}
 
-void ei_app_quit_request(void):
+void ei_app_invalidate_rect(ei_rect_t* rect) {
 
-ei_widget_t* ei_app_root_widget(void):
+}
+
+void ei_app_quit_request(void) {
+
+}
+
+ei_widget_t* ei_app_root_widget(void) {
         return NULL;
+}
 
-ei_surface_t ei_app_root_surface(void):
+ei_surface_t ei_app_root_surface(void) {
         return NULL;
+}
