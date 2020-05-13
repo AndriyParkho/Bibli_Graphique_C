a1534e5431ad2c9051cde0351ba0d274866fde98
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2020-05-13 15:58:25 +0200

    Ajout du fichier widgetfunc

---
+++ src/ei_widgetfunc.c
@@ -0,0 +1,9 @@
+/*
+ *  Fichier où il y aura toutes les fonctions des différents widgets
+ */
+
+void frame_draw(ei_widget_t* widget, ei_size_t frame_size, ei_color_t frame_color,
+                ei_relief_t frame_relief, int frame_border_width){
+        ei_frame_t* frame = (ei_frame_t*)widget;
+
+}
