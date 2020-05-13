ae11e3cedeb9755f6f5358778c79b5d3780b60be
morvanei <enora.morvan@outlook.com>
2020-05-13 18:48:39 +0200

    Oops

--- src/ei_widget_frame.c
+++ src/ei_widget_frame.c
@@ -3,16 +3,6 @@
  */
 #include "ei_draw.h"
 
-/**
- * \brief	A function that draws widgets of a class.
- *
- * @param	widget		A pointer to the widget instance to draw.
- * @param	surface		Where to draw the widget. The actual location of the widget in the
- *				surface is stored in its "screen_location" field.
- * @param	pick_surface	The surface used for picking (i.e. find the widget below the mouse pointer).
- * @param	clipper		If not NULL, the drawing is restricted within this rectangle
- *				(expressed in the surface reference frame).
- */
 void frame_draw(ei_widget_t* widget){
         ei_draw_polygon(main_window, points, transp_blue, NULL);
 }
