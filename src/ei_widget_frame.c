1a0e572f41761aaeb059402b7957ece7f1d75f15
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2020-05-13 16:45:05 +0200

    Fonction frame ajouté

--- src/ei_widget_frame.c
+++ src/ei_widget_frame.c
@@ -2,8 +2,7 @@
  *  Fichier où il y aura toutes les fonctions des différents widgets
  */
 
-void frame_draw(ei_widget_t* widget, ei_size_t frame_size, ei_color_t frame_color,
-                ei_relief_t frame_relief, int frame_border_width){
+void frame_draw(ei_widget_t* widget){
         ei_frame_t* frame = (ei_frame_t*)widget;
 
 }
