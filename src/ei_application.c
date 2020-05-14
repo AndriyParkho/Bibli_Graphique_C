dbe489c46a6051f5b6894f4aaba10e05f6d847ab
Thibault <thibault.launay@grenoble-inp.org>
2020-05-14 16:21:46 +0200

    draw

--- src/ei_application.c
+++ src/ei_application.c
@@ -17,7 +17,7 @@ void ei_app_free(void) {
 }
 
 void ei_app_run(void) {
-        ei_widget_t *widget; //jsp
+        ei_widget_t *widget = ei_app_root_widget(); //jsp
         hw_surface_lock(root_widget);
         widget->wclass->drawfunc(widget, root_widget, root_widget, NULL);
         hw_surface_unlock(root_widget);
