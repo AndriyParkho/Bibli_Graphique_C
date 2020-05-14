d68b568aebb3bb8fec36710fd6ed489932643a6b
Thibault <thibault.launay@grenoble-inp.org>
2020-05-14 16:36:33 +0200

    ajout bibliothèques

--- src/ei_widget_frame.c
+++ src/ei_widget_frame.c
@@ -1,12 +1,13 @@
 /*
  *  Fichier où il y aura toutes les fonctions des différents widgets
  */
-#include "../include/ei_draw.h"
+#include <ei_draw.h>
 #include <stdlib.h>
-#include "../ei_widget.h"
+#include <ei_widget.h>
+#include <ei_widgettypes.h>
 
 void frame_drawfunc(ei_widget_t* widget, ei_surface_t draw_surface, ei_surface_t pick_surface, ei_rect_t* clipper){
-        ei_fill(draw_surface, widget.pick_color, clipper);
+        ei_fill(draw_surface, widget->pick_color, clipper);
 }
 
 
