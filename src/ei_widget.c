3bc41cd79f51c7a7e9418a65c35d1f60c649026d
morvanei <enora.morvan@outlook.com>
2020-05-15 18:03:58 +0200

    Vérification d'img et text dans frame configure

--- src/ei_widget.c
+++ src/ei_widget.c
@@ -8,6 +8,7 @@ Fonction pour implémenter les widget
 #include <string.h>
 #include "ei_widgetclass_parcours.h"
 #include <stdlib.h>
+#include <assert.h>
 
 struct ei_geometry_param_t;
 struct ei_event_t;
@@ -158,22 +159,9 @@ void			ei_frame_configure		(ei_widget_t*		widget,
                                                                ei_surface_t*		img,
                                                                ei_rect_t**		img_rect,
                                                                ei_anchor_t*		img_anchor){
+    assert(!(text != NULL && img!=NULL));
     ei_frame_t* frame = (ei_frame_t*)widget;
-    frame->widget.wclass->setdefaultsfunc(frame);
-    //frame->widget->wclass = NULL; normalement dans ei_widget_create
-    //frame->widget.pick_id = NULL; normalement dans ei_widget_create
-    //pick_color à faire dans widget_create;
-    //frame->widget->user_date = NULL; normalement dans ei_widget_create
-    //frame->widget.destructor = NULL; normalement dans ei_widget_create
 
-    //frame->widget->parent = NULL; normalement dans ei_widget_create
-    //(*frame).widget.children_head = NULL;
-    //(*frame).widget.children_tail = NULL;
-    //(*frame).widget.next_sibling = NULL;
-    //Ici il faudra sûrement vérifier qui est le widget avant lui dans la hiérachie (son sibling d'avant)
-    //pour pouvoir rajouter ce widget au champ "next_sibling" de son previous sibling
-
-    //frame->widget->geom_params = NULL; j'ai pas compris mais je suppose dans widget_create
     if (requested_size) frame->widget.requested_size = *requested_size;
     //frame->widget.screen_location = NULL; pas compris mais sûrement widget_create
     //frame->widget->content_rect = &(frame->widget.screen_location);
