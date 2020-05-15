c4bf18f9604038f25b0287e9c446e9cf9dae1b58
Thibault <thibault.launay@grenoble-inp.org>
2020-05-15 10:16:09 +0200

    Test drawfunc

--- src/ei_widget_frame.c
+++ src/ei_widget_frame.c
@@ -1,16 +1,15 @@
 /*
  *  Fichier où il y aura toutes les fonctions des différents widgets
  */
-#include "ei_draw.h"
 #include <stdlib.h>
+#include "ei_draw.h"
+#include "ei_types.h"
 #include "ei_widget.h"
 #include "ei_widgettypes.h"
 
 void frame_drawfunc(ei_widget_t* widget, ei_surface_t draw_surface, ei_surface_t pick_surface, ei_rect_t* clipper){
         ei_fill(draw_surface, widget->pick_color, clipper);
 }
-
-
 
 void* frame_allocfunc(){
         return(calloc(1, sizeof(ei_frame_t)));
