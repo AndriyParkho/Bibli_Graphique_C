dca9657094d4b693532d2a061e6d8c8c3f404c88
morvanei <enora.morvan@outlook.com>
2020-05-18 11:29:45 +0200

    Rajout des accolades

---
+++ include/ei_geometrytypes.h
@@ -0,0 +1,16 @@
+//
+// Created by Enora on 18/05/2020.
+//
+
+#ifndef PROJETC_IG_EI_GEOMETRYTYPES_H
+#define PROJETC_IG_EI_GEOMETRYTYPES_H
+
+#include "SDL_keycode.h"
+#include "ei_types.h"
+#include "ei_geometrymanager.h"
+
+typedef struct {
+    ei_geometrymanager_t geomanager;
+} ei_placeur_t;
+
+#endif //PROJETC_IG_EI_GEOMETRYTYPES_H
