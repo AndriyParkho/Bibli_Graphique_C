0c0e140b5dfc566e4059f55f6404b78d6042133d
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2020-05-20 12:05:30 +0200

    Ptit changement

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -116,16 +116,16 @@ void			ei_draw_text		(ei_surface_t		surface,
  */
 void ei_fill (ei_surface_t surface, const ei_color_t* color, const ei_rect_t* clipper) {
         // clipper si non NULL sert à déterminer quel rectangle remplir
-        uint32_t *pixel_ptr = (uint32_t*)malloc(sizeof(uint32_t*));
         int i;
         ei_size_t surface_size = hw_surface_get_size(surface);
         //printf("%i \t %i \t %i \t %i \n", clipper->size.height, clipper->size.width, clipper->top_left.x, clipper->top_left.y);
         if (clipper) {
+                printf("avant : %i \n", hw_surface_get_buffer(surface));
                 hw_surface_set_origin(surface, clipper->top_left);
+                printf("après : %i \n", hw_surface_get_buffer(surface));
                 surface_size = clipper->size;
-                // printf("%i \n", );
         }
-        pixel_ptr = (uint32_t *) hw_surface_get_buffer(surface);
+        uint32_t *pixel_ptr = (uint32_t *) hw_surface_get_buffer(surface);
         printf("Adresse : %d - Valeur : %d\n",pixel_ptr, *pixel_ptr);
         for (i = 0; i < surface_size.height * surface_size.width; i++) {
                 *pixel_ptr = ei_map_rgba(surface, color);
