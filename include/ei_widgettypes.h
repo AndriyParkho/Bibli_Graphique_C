ec8499c165e7cca4174cf1f26927dd2eb08468cd
morvanei <enora.morvan@outlook.com>
2020-05-20 14:43:00 +0200

    Struct ei_button_t

--- include/ei_widgettypes.h
+++ include/ei_widgettypes.h
@@ -31,4 +31,26 @@
  } ei_frame_t;
 
 
+ /**
+  *
+  */
+typedef struct {
+    ei_widget_t         widget;
+
+    ei_color_t          color;
+    int			        border_width;
+    int			        corner_radius;
+    ei_relief_t		    relief;
+    char*			    text;
+    ei_font_t		    text_font;
+    ei_color_t		    text_color;
+    ei_anchor_t		    text_anchor;
+    ei_surface_t		img;
+    ei_rect_t*		    img_rect;
+    ei_anchor_t		    img_anchor;
+    ei_callback_t		callback;
+    void*			    user_param;
+} ei_button_t;
+
+
 #endif //EI_WIDGETTYPES_H
