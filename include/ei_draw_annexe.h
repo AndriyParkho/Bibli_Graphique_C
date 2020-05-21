93a5d0848bd5e9e5afcca8ba05f276609a5d6c78
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2020-05-21 15:55:00 +0200

    Ajout du fichier draw annexe

---
+++ include/ei_draw_annexe.h
@@ -0,0 +1,11 @@
+/*
+ * Fonctions servant à dessiner les widgets et leurs détails
+ */
+
+
+#ifndef EI_DRAW_ANNEXE_H
+#define EI_DRAW_ANNEXE_H
+
+#include "ei_draw.h"
+
+#endif
---
+++ src/ei_draw_annexe.c
@@ -0,0 +1,6 @@
+/*
+ * Fonctions servant à dessiner les widgets et leurs détails
+ */
+
+#include "ei_draw_annexe.h"
+
